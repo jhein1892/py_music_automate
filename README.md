6af00636812e2737b8105ec00e4b882ad6d8769d
Jacob Hein <jacobhein1892@gmail.com>
2023-12-29 13:36:28 -0800

    added some error handling for when the use trys the program for the first time or when the auth token is expired.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # py_music_automate
 A script to automatically download the top 10 songs from a music website, and organize them into folders
+
--- main.py
+++ main.py
@@ -1,59 +1,70 @@
-# This program is going to download the top 10 songs from a music website, whenever the program is run.
-# I don't want to download previously downloaded songs.
-# I want to store the downloaded music in a folder
-# I want to store the names of the songs in a file
-# I want to reference the day file and then pull the file from the music folder to play.
-
-
-# First, I need to find the API that is going to let me see and download the top 10 songs of the day.
-
-# curl -X POST "https://accounts.spotify.com/api/token" \
-#      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "grant_type=client_credentials&client_id=449201bb092d4a52adbcfcd97637dd94&client_secret=5dafd98e2e5c416498c9a2dfab8feacb"
-
-
-#{"access_token":"BQCSr684oDgilveGYq51-wF9RmJIbEQ_2zIkDAmnofjM3YYy50dBduTHyHHs_ojWWZ_5zYb2vkK9JCPwgAuyNH_Trm6lZwIvBgcNtFhUwG79JiG3Fds","token_type":"Bearer","expires_in":3600}
 import requests
 import json
+import datetime
 
-def authUser() -> str:
+# Saves New Auth Token to File
+def save_auth(auth_token):
+    with open('auth.txt', 'w') as file:
+            json.dump(auth_token, file, indent=2)
+
+# Run the Auth API to get new token
+def authUser():
     authUrl = 'https://accounts.spotify.com/api/token'
     headers = {
         "Content-Type": 'application/x-www-form-urlencoded'
     }
-    payload = {
-        'grant-type': 'client_credentials',
-        # These need to be hidden and moved
-        'client_id':'449201bb092d4a52adbcfcd97637dd94',
-        'client_secret': '5dafd98e2e5c416498c9a2dfab8feacb'
-    }
-    res = requests.post(url=authUrl, headers=headers, payload=payload)
 
+    with open('secrets.json') as json_file:
+         data = json.load(json_file)
+
+    payload = {
+        'grant_type': 'client_credentials',
+
+        # These need to be hidden and moved
+        'client_id': data['Credentials']['client_id'],
+        'client_secret': data['Credentials']['client_secret']
+    }
+    res = requests.post(url=authUrl, headers=headers, data=payload)
+    new_auth_json = res.json()
+    print(new_auth_json)
+    save_auth(new_auth_json)
+    # I should get a access token json from this
+
+# Returns Date string for file creation
+def getDate():
+    today = datetime.date.today()
+    return today
 
 # I'm going to check my auth code using this function
-with open('auth.txt', 'r') as file:
-    file_content = file.read()
-    data = json.loads(file_content)
+try:
+    with open('auth.txt', 'r') as file:
+        file_content = file.read()
+        data = json.loads(file_content)
+    access_token = data['access_token']
+    token_type = data['token_type']
+    url = 'https://api.spotify.com/v1/recommendations?seed_genres=hip-hop%2Ccountry'
+    headers = {
+        'Authorization': f"{token_type} {access_token}",
+    }
+    res = requests.get(url=url, headers=headers)
+    # If I get a successful response I want to see the response.
+    if res.status_code == 200:
+        res_json = res.json()
+        tracks = res_json['tracks']
+        recommended_tracks = dict()
+        for i, track in enumerate(tracks):
+            artist = track['artists'][0]['name']
+            title =  track['name']
+            recommended_tracks[i] = artist + ": " + title
 
-print(data)
+        todays_date = getDate()
+        with open(f'./recommendations/{todays_date}.txt', 'w') as file:
+            json.dump(recommended_tracks, file, indent=2)
 
-url = "https://api.spotify.com/v1/me"
-headers = {
-    'Authorization': 'Bearer BQCSr684oDgilveGYq51-wF9RmJIbEQ_2zIkDAmnofjM3YYy50dBduTHyHHs_ojWWZ_5zYb2vkK9JCPwgAuyNH_Trm6lZwIvBgcNtFhUwG79JiG3Fds',
-}
-res = requests.get(url=url, headers=headers)
-# If I get a successful response I want to see the response.
-if res.status_code == 200:
-    res_json = res.json()
-    print(res_json)
-
-else:
-    if res.status_code == 401:
-        new_auth = authUser()
-        with open('auth.txt', 'w') as file:
-            test_dict = {"Test": "Test"}
-            # test_json = test_dict.json()
-
-            json.dump(test_dict, file, indent=2)
-    print(f"Error: {res.status_code}")
-    print()
+    else:
+        if res.status_code == 401:
+            print("I need to get a new auth code")
+            authUser()
+except Exception as e:
+    print("Need some auth credentials")
+    authUser()
